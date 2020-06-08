95ab5e82e218f526a1497f931331c665262be5c4
egorbauer <egorbauer@yandex.ru>
2020-06-09 00:14:29 +0700

    Added textures in void menu(Menu.cpp)

---
+++ src/Menu.cpp
@@ -0,0 +1,12 @@
+#include "Menu.hpp"
+
+bool isMenu = 1;
+
+void menu(RenderWindow& window)
+{
+    Texture BackgroundTexture, ButtonInternetTexture, ButtonMobileTexture,
+            ButtonTvTexture, ButtonDevelopersTexture, ButtonExitTexture,
+            BackgroundInternetTexture, BackgroundMobileTexture,
+            BackgroundTvTexture,
+            BackgroundDevelopersTexture; // Nujno dobavit knopki Next and Back
+}
