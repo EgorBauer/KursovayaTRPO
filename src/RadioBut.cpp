9448ced12192e472bedb6d846923beb6606fc167
Kimot0 <nmoniev@gmail.com>
2020-06-17 23:51:49 +0700

    Initialized class Radio and add librarys

---
+++ src/RadioBut.cpp
@@ -0,0 +1,14 @@
+#ifndef H_RADIO
+#define H_RADIO
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <iostream>
+class radio {
+private:
+
+
+public:
+
+}
+#endif
