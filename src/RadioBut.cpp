61d7dab8586ac80b844d802baf6eb5da87dfdc7d
Kimot0 <nmoniev@gmail.com>
2020-06-18 14:49:32 +0700

    Initialize private variable menuNum and made setter and getter for it

--- src/RadioBut.cpp
+++ src/RadioBut.cpp
@@ -6,7 +6,19 @@
 #include <iostream>
 class radio {
 private:
+    int menuNum = 0;
+
 public:
+    void menuNumSet(int Value)
+    {
+        menuNum = Value;
+    }
+
+    int menuNumGet()
+    {
+        return menuNum;
+    }
+
     int draw(int x1, int x2, int x3, int y1, int y2, int y3)
     {
         Texture RadButtonTexture;
@@ -16,11 +28,7 @@ public:
         but1.setPosition(x1, y1);
         but2.setPosition(x2, y2);
         but3.setPosition(x3, y3);
-	        
-        
-        
-        
-        
+
         window.draw(but1);
         window.draw(but2);
         window.draw(but3);
