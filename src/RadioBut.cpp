4c8318594260933d8faac1f70166d2e95087ce1f
Kimot0 <nmoniev@gmail.com>
2020-06-18 14:46:42 +0700

    Created function draw with drawing radiobuttuns

--- src/RadioBut.cpp
+++ src/RadioBut.cpp
@@ -6,9 +6,24 @@
 #include <iostream>
 class radio {
 private:
-
-
 public:
-
+    int draw(int x1, int x2, int x3, int y1, int y2, int y3)
+    {
+        Texture RadButtonTexture;
+        RadButtonTexture.loadFromFile("image/rad.png");
+        Sprite but1(RadButtonTexture), but2(RadButtonTexture),
+                but3(RadButtonTexture);
+        but1.setPosition(x1, y1);
+        but2.setPosition(x2, y2);
+        but3.setPosition(x3, y3);
+	        
+        
+        
+        
+        
+        window.draw(but1);
+        window.draw(but2);
+        window.draw(but3);
+    }
 }
 #endif
