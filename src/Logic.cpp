589442753fb0380cbd1c06ad95e2e7f4bcf9b8df
Kimot0 <nmoniev@gmail.com>
2020-06-22 16:37:10 +0700

    Created internet_get_ans fucntion

--- src/Logic.cpp
+++ src/Logic.cpp
@@ -47,49 +47,109 @@ public:
 
     void internet_get_ans(radio button, radio button1, radio button2)
     {
+        std::fstream answer1("thirdparty/txt/inet.txt", std::ios::in);
+        for (int i = 0; i < 20; i++) {
+            std::string temp;
+            getline(answer1, temp);
+            GiveRes::parcer(temp);
+            int grr = 0;
+            int grl = 0;
+            int grr1 = 0;
+            int grl1 = 0;
+            int otv;
+
+            if (button.menuNumGet() == 1) {
+                grr = 0;
+                grl = 401;
+            }
+            if (button.menuNumGet() == 2) {
+                grr = 401;
+                grl = 601;
+            }
+            if (button.menuNumGet() == 3) {
+                grr = 601;
+                grl = 2000;
+            }
+            if (button1.menuNumGet() == 1) {
+                grr1 = 0;
+                grl1 = 51;
+            }
+            if (button1.menuNumGet() == 2) {
+                grr1 = 51;
+                grl1 = 101;
+            }
+            if (button1.menuNumGet() == 3) {
+                grr1 = 102;
+                grl1 = 1000;
+            }
+            if (ans3 == "YES") {
+                otv = 1;
+            }
+            if (ans3 == "NO") {
+                otv = 2;
+            }
+            if (ans3 == "IDC") {
+                otv = 3;
+            }
+
+            if (grr < ans1 && grl > ans1) {
+                if (grr1 < ans2 && grl1 > ans2) {
+                    if (otv == button2.menuNumGet()) {
+                        tariffs.push_back(temp);
+                    } else
+                        continue;
+                } else
+                    continue;
+            } else
+                continue;
+        }
     }
-    void mobile_get_ans(radio button, radio button1, radio button2)
-    {
+
+    answer1.close();
+}
+}
+void mobile_get_ans(radio button, radio button1, radio button2)
+{
+}
+void tv_get_ans(radio button, radio button1, radio button2)
+{
+}
+void parcer(std::string value)
+{
+    long unsigned i = 0;
+    std::string time;
+    char* pEnd;
+    name = "";
+    while (value[i] != ' ') {
+        name += value[i];
+        i++;
     }
-    void tv_get_ans(radio button, radio button1, radio button2)
-    {
+    while (value[i] == ' ') {
+        i++;
     }
-    void parcer(std::string value)
-    {
-        long unsigned i = 0;
-        std::string time;
-        char* pEnd;
-        name = "";
-        while (value[i] != ' ') {
-            name += value[i];
-            i++;
-        }
-        while (value[i] == ' ') {
-            i++;
-        }
-        while (value[i] != ' ') {
-            time += value[i];
-            i++;
-        }
-        ans1Set(strtol(time.c_str(), &pEnd, 10));
-        time = "";
-        while (value[i] == ' ') {
-            i++;
-        }
-        while (value[i] != ' ') {
-            time += value[i];
-            i++;
-        }
-        ans2 = strtol(time.c_str(), &pEnd, 10);
-        time = "";
-        while (value[i] == ' ') {
-            i++;
-        }
-        while (value[i] != ' ' && value[i] != '\0') {
-            time += value[i];
-            i++;
-        }
-        ans3 = time;
-        ans4 = strtol(time.c_str(), &pEnd, 10);
+    while (value[i] != ' ') {
+        time += value[i];
+        i++;
     }
+    ans1Set(strtol(time.c_str(), &pEnd, 10));
+    time = "";
+    while (value[i] == ' ') {
+        i++;
+    }
+    while (value[i] != ' ') {
+        time += value[i];
+        i++;
+    }
+    ans2 = strtol(time.c_str(), &pEnd, 10);
+    time = "";
+    while (value[i] == ' ') {
+        i++;
+    }
+    while (value[i] != ' ' && value[i] != '\0') {
+        time += value[i];
+        i++;
+    }
+    ans3 = time;
+    ans4 = strtol(time.c_str(), &pEnd, 10);
+}
 }
