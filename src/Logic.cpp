7f62e1e3f4a2a29555b829bc1fdda6e2e5e0bef8
Kimot0 <nmoniev@gmail.com>
2020-06-22 15:15:37 +0700

    Added 3 variables and setters with getters for them

--- src/Logic.cpp
+++ src/Logic.cpp
@@ -4,11 +4,34 @@
 
 class GiveRes {
 private:
-
+    int ans1, ans2;
+    std::string ans3;
 public:
+    void ans1Set(int Value)
+    {
+        ans1 = Value;
+    }
 
+    int ans1Get()
+    {
+        return ans1;
+    }
+    void ans2Set(int Value)
+    {
+        ans2 = Value;
+    }
 
+    int ans2Get()
+    {
+        return ans2;
+    }
+    void ans3Set(std::string Value)
+    {
+        ans3 = Value;
+    }
 
-
-
+    std::string ans3Get()
+    {
+        return ans3;
+    }
 }
